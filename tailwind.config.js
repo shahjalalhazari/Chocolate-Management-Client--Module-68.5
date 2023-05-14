f43148cde374e5cd051b130ae3e7b9229527acdc
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2023-05-14 18:54:22 +0400

    Project Created.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{html,js}',
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
